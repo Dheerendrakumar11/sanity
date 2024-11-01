96dbc6894e9136e738ff05d54309da1c3d627bc3
Dheerendarkumar11 <dhrajch@gmail.com>
2024-11-01 16:17:42 +0530

    first commit

--- schemaTypes/category.js
+++ schemaTypes/category.js
@@ -1,19 +1,41 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
+export default {
   name: 'category',
   title: 'Category',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
+    {
+      name: 'name',
+      title: 'Name',
       type: 'string',
-    }),
-    defineField({
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
       name: 'description',
       title: 'Description',
       type: 'text',
-    }),
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'url',
+    },
+    {
+      name: 'count',
+      title: 'Count',
+      type: 'number',
+    },
+    {
+      name: 'parent',
+      title: 'Parent Category ID',
+      type: 'number',
+    },
   ],
-})
+};
--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,4 +1,4 @@
-import blockContent from './blockContent'
+// import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
